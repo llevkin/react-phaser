099ce3593d44cb72ffe302e1e796dbcc5407396b
llevkin <llevkin@playflock.com>
2016-11-14 12:40:05 +0300

    updates

--- src/impl/input/input.js
+++ src/impl/input/input.js
@@ -16,13 +16,13 @@ function callback(pointer) {
     prevY = pointer.y;
 }
 
-// function onUp(pointer) {
-//     prevX = prevY = 0;
-//     pointer.cX = 0;
-//     pointer.cY = 0;
-//     pointer.dX = 0;
-//     pointer.dY = 0;
-// }
+function clearPrev(pointer) {
+    prevX = prevY = 0;
+    // pointer.cX = 0;
+    // pointer.cY = 0;
+    // pointer.dX = 0;
+    // pointer.dY = 0;
+}
 
 function Input() {
     this.super(Input, 'constructor', arguments);
@@ -39,14 +39,14 @@ Input.prototype.update = function(prevProps) {
         return;
     this.root.obj.input.deleteMoveCallback(callback);
     this.root.obj.input.addMoveCallback(callback);
-    // if ('onInputUp' in this.root.obj.input) {
-    //     this.root.obj.input.onInputUp.remove(onUp);
-    //     this.root.obj.input.onInputUp.add(onUp);
-    // }
-    // else if ('onUp' in this.root.obj.input) {
-    //     this.root.obj.input.onUp.remove(onUp);
-    //     this.root.obj.input.onUp.add(onUp);
-    // }
+    if ('onInputUp' in this.root.obj.input) {
+        this.root.obj.input.onInputUp.remove(clearPrev);
+        this.root.obj.input.onInputUp.add(clearPrev);
+    }
+    else if ('onUp' in this.root.obj.input) {
+        this.root.obj.input.onUp.remove(clearPrev);
+        this.root.obj.input.onUp.add(clearPrev);
+    }
     if (this.props.name === 'moveCallback') {
         if (prevProps)
             this.root.obj.input.deleteMoveCallback(prevProps.callback);
