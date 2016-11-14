e2eb9844fdfcd93d449433c030615828bf3feeb0
llevkin <llevkin@playflock.com>
2016-11-14 11:05:45 +0300

    updates

--- src/impl/input/input.js
+++ src/impl/input/input.js
@@ -16,6 +16,22 @@ function callback(pointer) {
     prevY = pointer.y;
 }
 
+function onUp(pointer) {
+    prevX = prevY = 0;
+    pointer.cX = 0;
+    pointer.cY = 0;
+    pointer.dX = 0;
+    pointer.dY = 0;
+}
+
+function onDown(pointer) {
+    prevX = prevY = 0;
+    pointer.cX = 0;
+    pointer.cY = 0;
+    pointer.dX = 0;
+    pointer.dY = 0;
+}
+
 function Input() {
     this.super(Input, 'constructor', arguments);
 }
@@ -31,6 +47,16 @@ Input.prototype.update = function(prevProps) {
         return;
     this.root.obj.input.deleteMoveCallback(callback);
     this.root.obj.input.addMoveCallback(callback);
+    if ('onInputUp' in this.root.obj.input) {
+        this.root.obj.input.onInputUp.remove(onUp);
+        this.root.obj.input.onInputDown.remove(onDown);
+        this.root.obj.input.onInputUp.add(onUp);
+    }
+    else if ('onUp' in this.root.obj.input) {
+        this.root.obj.input.onUp.remove(onUp);
+        this.root.obj.input.onDown.remove(onDown);
+        this.root.obj.input.onUp.add(onUp);
+    }
     if (this.props.name === 'moveCallback') {
         if (prevProps)
             this.root.obj.input.deleteMoveCallback(prevProps.callback);
