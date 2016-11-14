071725f3516732b3ec9b5a1d40f88a41fe17af55
llevkin <llevkin@playflock.com>
2016-11-14 11:46:42 +0300

    updates

--- src/impl/input/input.js
+++ src/impl/input/input.js
@@ -24,13 +24,13 @@ function onUp(pointer) {
     // pointer.dY = 0;
 }
 
-function onDown(pointer) {
-    prevX = prevY = 0;
+// function onDown(pointer) {
+//     prevX = prevY = 0;
     // pointer.cX = 0;
     // pointer.cY = 0;
     // pointer.dX = 0;
     // pointer.dY = 0;
-}
+// }
 
 function Input() {
     this.super(Input, 'constructor', arguments);
@@ -49,12 +49,12 @@ Input.prototype.update = function(prevProps) {
     this.root.obj.input.addMoveCallback(callback);
     if ('onInputUp' in this.root.obj.input) {
         this.root.obj.input.onInputUp.remove(onUp);
-        this.root.obj.input.onInputDown.remove(onDown);
+        // this.root.obj.input.onInputDown.remove(onDown);
         this.root.obj.input.onInputUp.add(onUp);
     }
     else if ('onUp' in this.root.obj.input) {
         this.root.obj.input.onUp.remove(onUp);
-        this.root.obj.input.onDown.remove(onDown);
+        // this.root.obj.input.onDown.remove(onDown);
         this.root.obj.input.onUp.add(onUp);
     }
     if (this.props.name === 'moveCallback') {
