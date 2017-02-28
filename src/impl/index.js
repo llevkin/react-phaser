b0943483818ae7f482906d2833b229ead4097590
llevkin <llevkin@playflock.com>
2017-02-28 13:38:44 +0300

    update

--- src/impl/index.js
+++ src/impl/index.js
@@ -1,19 +1,20 @@
 module.exports = {
 
-    node:       require('./node'),
-    game:       require('./game'),
-    state:      require('./state'),
-    sprite:     require('./sprite'),
-    tilesprite: require('./tilesprite'),
-    group:      require('./group'),
+    node:           require('./node'),
+    game:           require('./game'),
+    state:          require('./state'),
+    sprite:         require('./sprite'),
+    tilesprite:     require('./tilesprite'),
+    group:          require('./group'),
+    spritebatch:    require('./spritebatch'),
     // ui:         require('./ui'),
-    animation:  require('./animation'),
-    graphics:   require('./graphics'),
-    text:       require('./text'),
+    animation:      require('./animation'),
+    graphics:       require('./graphics'),
+    text:           require('./text'),
 
-    events:     require('./input/events'),
-    input:      require('./input/input'),
-    key:        require('./input/key'),
-    tween:      require('./tween'),
+    events:         require('./input/events'),
+    input:          require('./input/input'),
+    key:            require('./input/key'),
+    tween:          require('./tween'),
 
 };
---
+++ src/impl/spritebatch.js
@@ -0,0 +1,94 @@
+var util            = require('util');
+var DisplayObject   = require('./displayObject');
+
+var events = ['onChildInputDown', 'onChildInputOut', 'onChildInputOver', 'onChildInputUp'];
+
+function SpriteBatch() {
+    this.super(this.constructor, 'constructor', arguments);
+}
+util.inherits(SpriteBatch, DisplayObject);
+
+SpriteBatch.prototype.init = function() {
+    this.obj = new Phaser.SpriteBatch(this.game);
+    this.super(this.constructor, 'init');
+};
+
+SpriteBatch.prototype.update = function(prevProps) {
+    var event;
+    if (!this.obj)
+        return;
+    if ('inputEnableChildren' in this.props)
+        this.obj.inputEnableChildren = this.props.inputEnableChildren;
+    'visible' in this.props && (this.obj.visible = this.props.visible);
+    for (var i = 0; i < events.length; i++) {
+        event = events[i];
+        if (event in this.props) {
+            if (prevProps && event in prevProps)
+                this.obj[event].remove(prevProps[event]);
+            this.obj[event].add(this.props[event]);
+        }
+    }
+    this.super(this.constructor, 'update');
+};
+
+Object.defineProperties(SpriteBatch.prototype, {
+
+    x: {
+        get: function() {
+            var percent;
+            if (!this.obj)
+                return 0;
+            if (!('x' in this.props))
+                return this.parentNode.x;
+            if (!~String(this.props.x).indexOf('%'))
+                return parseInt(this.props.x, 10);
+            percent = parseInt(this.props.x, 10);
+            return this.parentNode.x * percent / 100;
+        }
+    },
+
+    y: {
+        get: function() {
+            var percent;
+            if (!this.obj)
+                return 0;
+            if (!('y' in this.props))
+                return this.parentNode.y;
+            if (!~String(this.props.y).indexOf('%'))
+                return parseInt(this.props.y, 10);
+            percent = parseInt(this.props.y, 10);
+            return this.parentNode.y * percent / 100;
+        }
+    },
+
+    width: {
+        get: function() {
+            var percent;
+            if (!this.obj)
+                return 0;
+            if (!('width' in this.props))
+                return this.parentNode.width;
+            if (!~String(this.props.width).indexOf('%'))
+                return parseInt(this.props.width, 10);
+            percent = parseInt(this.props.width, 10);
+            return this.parentNode.width * percent / 100;
+        }
+    },
+
+    height: {
+        get: function() {
+            var percent;
+            if (!this.obj)
+                return 0;
+            if (!('height' in this.props))
+                return this.parentNode.height;
+            if (!~String(this.props.height).indexOf('%'))
+                return parseInt(this.props.height, 10);
+            percent = parseInt(this.props.height, 10);
+            return this.parentNode.height * percent / 100;
+        }
+    },
+
+});
+
+module.exports = SpriteBatch;
